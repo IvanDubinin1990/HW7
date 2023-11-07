93099b8b94eb4a2d8b33818cba890017cce1c5a9
Кристина <dubininivan1990@gmail.com>
2023-11-07 13:30:58 +0400

    Выполнено задание с помощью цикла while

--- Task 2/src/Main.java
+++ Task 2/src/Main.java
@@ -2,10 +2,11 @@ public class Main {
     public static void main(String[] args) {
         //todo Тут увольняем айтишников
 
-        for (int i = 1; i <= 100 ; i++) {
-            i++;
-            System.out.println("Уволен айтишник с id " + i);
+       int id = 2;
 
-        }
+       while (id <= 100){
+           System.out.println("Уволен айтишник с id " + id);
+           id = id + 2;
+       }
     }
 }
